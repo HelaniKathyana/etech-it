e08d45715119713a64a27d3bf9aa4dd1e378b398
HelaniW <helani.waidyarathne@xitricon.com>
2024-07-22 08:44:53 +0530

    add drift chat

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,19 @@
 import '../css/main.css';
 import { WithNinetailedProvider } from '../utils/ninetailed-helpers';
-
+import Script from 'next/script'
+ 
+export default function Page() {
+  return (
+    <>
+      <Script
+        src="./drift.js"
+        onLoad={() => {
+          console.log('Script has loaded')
+        }}
+      />
+    </>
+  )
+}
 export default function MyApp({ Component, pageProps }) {
     return (
         <WithNinetailedProvider>
