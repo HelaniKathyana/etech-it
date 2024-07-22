2446d44f4ad0c4883d0850dcc5c12f31b0a34ddc
HelaniW <helani.waidyarathne@xitricon.com>
2024-07-22 09:12:11 +0530

    updated

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import '../css/main.css';
 import { WithNinetailedProvider } from '../utils/ninetailed-helpers';
 import Script from 'next/script'
+import Drift from "react-driftjs";
  
 export default function MyApp({ Component, pageProps }) {
     return (
