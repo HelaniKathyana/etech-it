1d674bfe59a886858c0dc83a3bdeb46f0d432667
HelaniW <helani.waidyarathne@xitricon.com>
2024-07-22 08:49:41 +0530

    commited

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,22 +2,19 @@ import '../css/main.css';
 import { WithNinetailedProvider } from '../utils/ninetailed-helpers';
 import Script from 'next/script'
  
-export default function Page() {
-  return (
-    <>
+export default function MyApp({ Component, pageProps }) {
+    return (
+        <>
       <Script
         src="./drift.js"
         onLoad={() => {
           console.log('Script has loaded')
         }}
       />
-    </>
-  )
-}
-export default function MyApp({ Component, pageProps }) {
-    return (
-        <WithNinetailedProvider>
+      <WithNinetailedProvider>
             <Component {...pageProps} />
         </WithNinetailedProvider>
+    </>
+        
     );
 }
