d63c81c022003bb13dc0bd0cfc3f9ab77819aa95
Helani Waidyarathne <35195335+HelaniKathyana@users.noreply.github.com>
2025-01-18 08:14:08 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,31 @@
 import '../css/main.css';
 import { WithNinetailedProvider } from '../utils/ninetailed-helpers';
-import Script from 'next/script'
 import Drift from "react-driftjs";
- 
+import Script from 'next/script';
+
 export default function MyApp({ Component, pageProps }) {
     return (
-    <>
-    <Drift appId="2g8vxhkczgcv" />
-    <WithNinetailedProvider>
-            <Component {...pageProps} />
-        </WithNinetailedProvider>
-    </>
-      
-        
+        <>
+            <Script
+                strategy="afterInteractive"
+                src="https://www.googletagmanager.com/gtag/js?id=AW-16576091880"
+            />
+            <Script
+                id="gtag-init"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'AW-16576091880');
+                    `,
+                }}
+            />
+            <Drift appId="gwehber5pp8p" />
+            <WithNinetailedProvider>
+                <Component {...pageProps} />
+            </WithNinetailedProvider>
+        </>
     );
 }
