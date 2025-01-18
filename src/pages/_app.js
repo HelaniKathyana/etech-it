68199d9b0b36aeaea1fe4d6631602b1cab529ae7
Helani Waidyarathne <35195335+HelaniKathyana@users.noreply.github.com>
2025-01-18 21:14:22 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import '../css/main.css';
 import { WithNinetailedProvider } from '../utils/ninetailed-helpers';
 import Drift from "react-driftjs";
+import { GoogleTagManager } from '@next/third-parties/google';
 
 export default function MyApp({ Component, pageProps }) {
     return (
         <>
+            <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID} />
             <Drift appId="gwehber5pp8p" />
             <WithNinetailedProvider>
                 <Component {...pageProps} />
